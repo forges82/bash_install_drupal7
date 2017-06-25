a61436760a7c56ff99807a6fb1a3c1c6b8ec2782
Jorge Puente García <forges82@gmail.com>
2017-06-25 12:30:17 +0200

    Update install_drupal7.sh

--- install_drupal7.sh
+++ install_drupal7.sh
@@ -5,7 +5,7 @@ mv drupal/.htaccess .
 mv drupal/.editorconfig .
 mv drupal/.gitignore .
 rmdir drupal
-drush site-install standard --db-url='mysql://[db_user]:[db_pass]@localhost/[db_name]' --site-name=Example --account-name=admin --account-pass=[useruser_pass]
+drush7 site-install standard --db-url='mysql://[db_user]:[db_pass]@localhost/[db_name]' --site-name=Example --account-name=admin --account-pass=[useruser_pass]
 drush7 dis toolbar overlay --y
 drush7 en admin_menu module_filter admin_views coffee fpa zen --y
 drush7 cc all
@@ -22,6 +22,6 @@ drush7 dl l10n_update && drush7 en -y $_
 drush7 language-add es && drush7 language-enable $_
 drush7 l10n-update-refresh
 drush7 l10n-update
-drush cc all
+drush7 cc all
 drush7 language-default es
 drush7 language-disable en
